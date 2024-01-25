938debbf743ab7b441af86d5081d09fce8598ba1
choiIsabelle <isabellelissina@gmail.com>
2024-01-24 23:05:54 -0500

    styling updates

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -6,14 +6,16 @@ const DatepickerContainer = styled.div`
   width: 40%;
   box-shadow: 0 2px 9px rgba(0, 0, 0, 0.2);
   display: flex;
-  padding: 2rem;
+  padding:1rem;
+  width: 35%;
+  height: 20%;
   background-color: white;
   justify-content: center;
   border-radius: 25px;
   margin: auto;
   align-items: center;
   flex-direction: column;
-  gap: 2rem;
+  gap: 0.4rem;
 `;
 
 const Title = styled.h3`
@@ -25,7 +27,24 @@ const EmphTitle = styled.h3`
 font-weight: bold;
 font-size: 19px;
 padding: 0.5rem;
-color: green;
+color: blue;
+`
+
+const SubButton = styled.button`
+display:inline-block;
+padding:0.3em 1.2em;
+margin:0 0.1em 0.1em 0;
+background-color: blue;
+border:0.3rem solid blue;
+border-radius:2em;
+box-sizing: border-box;
+text-decoration:none;
+font-family:'Roboto',sans-serif;
+font-weight:300;
+color:white;
+text-shadow: 0 0.04em 0.04em rgba(0,0,0,0.35);
+text-align:center;
+outline: none;
 `
 
 export const Datepicker=(props)=> {
@@ -59,6 +78,7 @@ export const Datepicker=(props)=> {
           onMonthChange={handleMonthChange}
           selected={selectedDates}
         />
+        <SubButton>Submit</SubButton>
       </DatepickerContainer>
 
     </>
