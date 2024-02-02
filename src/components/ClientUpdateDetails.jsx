c5400889bc362e2cf4bbef46da7f2ea3f1daae40
choiIsabelle <isabellelissina@gmail.com>
2024-02-02 12:57:03 -0500

    expand client options

--- src/components/ClientUpdateDetails.jsx
+++ src/components/ClientUpdateDetails.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import { Grid, Card } from './CustomComponents';
+import styled from 'styled-components'
+import * as CustomComponents from './CustomComponents';
 import { CondensedInput } from './CondensedInput';
+import {LockIcon} from './icons/LockIcon'
+import {PersonIcon} from './icons/PersonIcon'
+import { useState } from 'react';
+import { SpinnerOnSubmit } from './SpinnerOnSubmit';
 
 //TODO: finish this for the client view of updating their personal information or payment information
 
+const IconContainer = styled.div`
+justify-content: center;
+display: flex;
+`
+
 export const ClientUpdateDetails=()=>{
+    const [foundAccount, setFoundAccount] = useState(false);
+    const [showDetails, setShowDetails] = useState(false);
+    const [isSearching, setIsSearching] = useState(false);
+    const handleFoundAccount=()=>{
+        setIsSearching(true)
+        setTimeout(()=>{
+            setIsSearching(false)
+            setShowDetails(true)
+            setFoundAccount(true)
+          }, 1500)
+        
+
+    }
+
     return(
-        <Grid>
-            <Card>
-<CondensedInput
-title={"Update your payment details"}
-subMsg={"Search for your account using your SSN"}
->
-</CondensedInput>
-            </Card>
-        </Grid>
+        <CustomComponents.Grid>
+           {!showDetails &&  <CustomComponents.Card>
+                <IconContainer>
+                {!foundAccount && <LockIcon></LockIcon>}
+                </IconContainer>
+        <CondensedInput
+        msg={"Search for your account using your SSN"}
+        title={"Locate your account using your SSN"}
+        subMsg={"Enter your 8-digit SSN"}
+        >
+        </CondensedInput>
+        <CustomComponents.SearchButton
+        onClick={handleFoundAccount}
+        >Search
+        </CustomComponents.SearchButton>
+        {isSearching && <SpinnerOnSubmit></SpinnerOnSubmit>}
+            </CustomComponents.Card> }
+           { foundAccount && <div><CustomComponents.Card>
+            Welcome, User
+            <IconContainer>
+            {foundAccount && <PersonIcon></PersonIcon>}
+            </IconContainer>
+            Update payment details
+
+
+            </CustomComponents.Card>
+            <CustomComponents.Card>
+            Update user details
+
+
+            </CustomComponents.Card>
+            </div>
+            }
+            
+        </CustomComponents.Grid>
     )
 }
--- src/components/CustomComponents.jsx
+++ src/components/CustomComponents.jsx
@@ -14,10 +14,31 @@ transition: 0.3s;
 display: flex;
 flex-direction: column;
 width: 400px;
-height: 600px;
+height: fit-content;
 padding: 1rem;
 margin-top: 2rem;
 margin-left: 2rem;
 border-radius: 10px;
+`
+
+export const SearchButton = styled.button`
+background-image: linear-gradient(to right, #77A1D3 0%, #79CBCA  51%, #77A1D3  100%);
+padding: 0.6rem;
+margin-top: 0.3rem;
+text-align: center;
+text-transform: uppercase;
+transition: 0.5s;
+background-size: 200% auto;
+color: white;            
+box-shadow: 0 0 20px #eee;
+border-radius: 10px;
+display: block;
+text-decoration: none;
+background-position: right center;
+
+&:hover {
+    background-position: left center;
+
+  }
 `
 
