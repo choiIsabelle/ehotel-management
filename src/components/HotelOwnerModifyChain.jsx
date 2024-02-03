946efc24624774c185965bce1bb9d9b164b8df58
choiIsabelle <isabellelissina@gmail.com>
2024-02-03 14:44:52 -0500

    subcomponent shouldn't be rendering two different components, let the upper level component handle that

--- src/components/HotelOwnerModifyChain.jsx
+++ src/components/HotelOwnerModifyChain.jsx
@@ -1,19 +1,8 @@
 import * as c from './CustomComponents'
 import {CondensedInput} from './CondensedInput'
-import {useState} from 'react'
-import { HotelOwnerAddChain } from './HotelOwnerAddChain'
 
 export const HotelOwnerModifyChain=()=>{
-
-    const [page, setPage]=useState('manage')
-
-    const handleNavigation=(currentPage)=>{
-        setPage(currentPage)
-    }
-
     return(
-        <div>
-            { page === 'manage' &&
         <div>
         <c.Title>Manage a Hotel Chain</c.Title>
         <c.Card>
@@ -25,14 +14,6 @@ export const HotelOwnerModifyChain=()=>{
             
         </c.Card>
         </div>
-        }
 
-        {page ==='add' &&
-        <HotelOwnerAddChain
-        
-        ></HotelOwnerAddChain>
-        }
-
-        </div>
     )
 }
