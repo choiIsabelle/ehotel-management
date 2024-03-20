ff79555d4104498cf3f2bdc8137e2af1cd910bfb
choiIsabelle <isabellelissina@gmail.com>
2024-03-20 14:52:09 -0400

    add http request for retreiving customer name and address

--- src/components/ClientUpdateUserDetails.jsx
+++ src/components/ClientUpdateUserDetails.jsx
@@ -1,25 +1,86 @@
 import * as Custom from './CustomComponents'
 import { CondensedInput } from './CondensedInput'
-export const ClientUpdateUserDetails=()=>{
+import {useState, useEffect} from 'react';
+
+export const ClientUpdateUserDetails=({id})=>{
+    const [customerName, setCustomerName ]= useState('current');
+    const [customerAddress, setCustomerAddress] = useState('currentAddress');
+
+    useEffect(() => {
+        const getCustomerUserDetails = async (id) => {
+            try {
+                const response = await fetch(`http://localhost:5000/customer/${id}`, {
+                    method: "GET"
+            });
+                const jsonData = await response.json();
+                const fullName = jsonData.rows[0].full_name;
+                const address = jsonData.rows[0].address;
+                setCustomerName(fullName);
+                setCustomerAddress(address);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getCustomerUserDetails(id);
+    }, [id]); 
+
+    const handleUpdateClientName= async (customerName)=>{
+        try {
+            const response = await fetch(`http://localhost:5000/customer/${id}`, {
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({full_name: customerName, address: customerAddress})
+            });
+            console.log(response)
+            
+        } catch (error) {
+            console.error(`Could not update customer with SSN ${id} `, error.message)   
+        }
+    }
+
+    const handleUpdateClientAddress= async (customerAddress)=>{
+        try {
+            const response = await fetch(`http://localhost:5000/customer/address/${id}`, {
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({address: customerAddress, full_name: customerName})
+            });
+            
+        } catch (error) {
+            console.error(`Could not update customer with SSN ${id} `, error.message)   
+        }
+    }
+
     return(
         <Custom.Card>
-        <form>
+            <Custom.Title>Welcome, {customerName}!</Custom.Title>
         <CondensedInput
         title={"Update User Details"}
         msg={"Update Name"}
+        handleClick={()=>setCustomerName('')}
+        handleChange={(e)=>setCustomerName(e.target.value)}
+        valueLabel={customerName}
         subMsg={'Enter the name you want associated with your profile'}
         >
         </CondensedInput>
-        <Custom.SearchButton>Update Name</Custom.SearchButton>
+        <Custom.SearchButton
+        id="ClientUpdateUserDetails-update-btn"
+        onClick={()=>handleUpdateClientName(customerName)}
+        >Update Name
+        </Custom.SearchButton>
         <CondensedInput
         msg={"Update Address"}
         subMsg={'Enter the address you want associated with your profile'}
+        valueLabel={customerAddress}
+        handleClick={()=> setCustomerAddress('')}
+        handleChange={(e)=> setCustomerAddress(e.target.value)}
         >
         </CondensedInput>
-        <Custom.SearchButton>Update Address</Custom.SearchButton>
-        </form>
-        
+        <Custom.SearchButton
+        id="ClientUpdateUserDetails-update-address-btn"
+        onClick={()=>handleUpdateClientAddress(customerAddress)}
+        >Update Address</Custom.SearchButton>
     </Custom.Card>
-
     )
 }
