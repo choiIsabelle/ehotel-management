256b8ed368354dd57c73947ac214d3533106997c
choiIsabelle <isabellelissina@gmail.com>
2024-03-26 15:37:56 -0400

    update SQL queries

--- src/backend/HotelManagementSystem.sql
+++ src/backend/HotelManagementSystem.sql
@@ -1,66 +1,69 @@
-CREATE TABLE Hotel (
-    hotelId VARCHAR(255) PRIMARY KEY,
+CREATE TABLE Hotel_chain (
+    hotel_chain_id VARCHAR(255) PRIMARY KEY,
     email_address VARCHAR(255),
     num_associated_hotels INTEGER,
     central_office_street_address VARCHAR(255),
-    central_office_address VARCHAR(255),
+    hotel_location VARCHAR(255),
     phone_number VARCHAR(20)
 );
 
-
-CREATE TABLE Hotel_chain (
-    hotel_chain_id SERIAL PRIMARY KEY,
+CREATE TABLE Hotel (
+    hotel_id SERIAL PRIMARY KEY,
+    hotel_name VARCHAR(255),
     manager VARCHAR(255),
     chain_phone_number VARCHAR(20),
     num_rooms INTEGER,
     hotel_address VARCHAR(255),
-    hotel_chain_name VARCHAR(255),
-    hotelChainId VARCHAR(255) REFERENCES Hotel(hotelId)
+    hotel_related_chain VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id)
 );
 
 
 CREATE TABLE Room (
-    roomNumber SERIAL PRIMARY KEY,
+    room_number SERIAL PRIMARY KEY,
     extendable BOOLEAN,
     room_capacity INTEGER,
     room_type VARCHAR(255),
-    daily_price DECIMAL(10, 2),
+    daily_rate DECIMAL(10, 2),
     amenities VARCHAR(255),
-    damages TEXT
-roomHotelChainId  VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id)
+    damages TEXT,
+room_hotel_chain_id INTEGER REFERENCES Hotel_chain(hotel_chain_id)
 );
 
 CREATE TABLE Booking (
-    reservation_id SERIAL PRIMARY KEY,
-    employee_SSN VARCHAR(255),
-    isARental BOOLEAN,
-    employee_name VARCHAR(255),
-    date DATE,
-    bookingHotelChainId VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id )
+    booking_id SERIAL PRIMARY KEY,
+    customer_SSN VARCHAR(255) REFERENCES customer(SSN),
+    employee_SSN VARCHAR(255) REFERENCES employee(SSN),
+    is_a_rental BOOLEAN,
+    departure_date DATE,
+    arrival_date DATE,
+    room_of_booking VARCHAR(255) REFERENCES Room(room_number),
+    booking_hotel_chain_id VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id )
 );
-
-
 
 CREATE TABLE Rental (
     rental_id SERIAL PRIMARY KEY,
-    reservation_id INTEGER REFERENCES Booking(reservation_id),
-    rental_duration INTERVAL,
-    rental_amount DECIMAL(10, 2),
-    room_number VARCHAR(255) REFERENCES Room(roomNumber),
-roomHotelChainId VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id)
+    customer_SSN VARCHAR(255) REFERENCES customer(SSN),
+    employee_SSN VARCHAR(255) REFERENCES employee(SSN),
+    is_a_rental BOOLEAN,
+    departure_date DATE,
+    arrival_date DATE,
+    room_of_booking VARCHAR(255) REFERENCES Room(room_number),
+    booking_hotel_chain_id VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id )
 );
 
 CREATE TABLE Customer (
     SSN VARCHAR(255) PRIMARY KEY,
     full_name VARCHAR(255),
-    date_of_registration DATE,
+    age INTEGER,
+    date_of_registration DATE NOT NULL DEFAULT current_timestamp,
     address VARCHAR(255)
 );
 
 CREATE TABLE Employee (
     SSN VARCHAR(255) PRIMARY KEY,
-    employeeName VARCHAR(255),
-    Address VARCHAR(255)
+    full_name VARCHAR(255),
+    work_place_id VARCHAR(255) REFERENCES Hotel_chain(hotel_chain_id),
+    address VARCHAR(255)
 );
 
 CREATE TABLE CUSTOMER_PAYMENT (
