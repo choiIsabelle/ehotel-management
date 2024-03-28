c81191f4456007208510198622ac8e1b38fcd580
choiIsabelle <isabellelissina@gmail.com>
2024-03-28 09:56:57 -0400

    update styling for back button icon chevron

--- src/components/ClientChevronLeftButton.jsx
+++ src/components/ClientChevronLeftButton.jsx
@@ -1,6 +1,8 @@
 import ChevronLeftIcon from './icons/ChevronLeftIcon'
 const buttonStyle={
-    backgroundColor: '#d7d7d7'
+    backgroundColor: '#d7d7d7',
+    marginTop:'1rem',
+    marginBottom:'1rem'
 }
 
 export const ClientGoBackChevronButton=({handleClick})=>{
